53239fac749e8494240a8e6a05683250ed824af2
Binu G <sunbkg@yahoo.com>
2020-10-19 22:27:28 +0530

    UI changes

--- corrsUi/src/app/api-service/ApiService.ts
+++ corrsUi/src/app/api-service/ApiService.ts
@@ -40,76 +40,14 @@ export class RestAPIService {
 
   EnableReasonSave(data:any):Observable<any>{
     return this.httpClient.put(this.api+'/AddReasonCode/Reasoncodeupdate',data)
-    // .pipe(
-    //           catchError(this.error)
-    //         )
   }
 
-  // error(error: HttpErrorResponse) {
-  //       let errorMessage = '';
-  //       if (error.error instanceof ErrorEvent) {
-  //         errorMessage = error.error.message;
-  //       } else {
-  //         errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
-  //       }
-  //       console.log(errorMessage);
-  //       return throwError(errorMessage);
-  //     }
+  GetPlantData():Observable<any>{
+    return this.httpClient.get(this.api+'/Corrsplants/GetAllPlantID')
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-  
-//   }
-//   // Create
-//   createTask(): Observable<any> {
-//     let API_URL = `${this.apiUrl}/create-task`;
-//     return this.http.post(API_URL, data)
-//       .pipe(
-//         catchError(this.error)
-//       )
-//   }
-
-//   // Read
-//   showTasks() {
-//     return this.http.get(`${this.apiUrl}`);
-//   }
-
-//   // Update
-//   updateTask(id, data): Observable<any> {
-//     let API_URL = `${this.apiUrl}/update-task/${id}`;
-//     return this.http.put(API_URL, data, { headers: this.headers }).pipe(
-//       catchError(this.error)
-//     )
-//   }
-
-//   // Delete
-//   deleteTask(id): Observable<any> {
-//     var API_URL = `${this.apiUrl}/delete-task/${id}`;
-//     return this.http.delete(API_URL).pipe(
-//       catchError(this.error)
-//     )
-//   }
-
-//   // Handle Errors 
-//   error(error: HttpErrorResponse) {
-//     let errorMessage = '';
-//     if (error.error instanceof ErrorEvent) {
-//       errorMessage = error.error.message;
-//     } else {
-//       errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
-//     }
-//     console.log(errorMessage);
-//     return throwError(errorMessage);
-//   }
+  EnablePlants(data:any):Observable<any>{
+    return this.httpClient.put(this.api+'/Corrsplants/plantidUpdate', data)
+  }
 
 }
